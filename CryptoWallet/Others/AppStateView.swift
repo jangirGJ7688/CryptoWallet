729975ba3a87ea2b8ba963f8b73dc6cc51cbdd8d
Ganpat Jangir <ganpatlaljangid7688@gmail.com>
2024-08-24 13:10:09 +0530

    reverted

---
+++ CryptoWallet/Others/AppStateView.swift
@@ -0,0 +1,18 @@
+//
+//  AppStateView.swift
+//  CryptoWallet
+//
+//  Created by Ganpat Jangir on 18/08/24.
+//
+
+import SwiftUI
+
+struct AppStateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppStateView()
+}
